0c4cf48ffd78a5459fe7395248c425e01564d9ea
Karthik Rangasai <karthikrangasai@gmail.com>
2021-10-25 15:33:11 +0530

    Add Dataset.

--- code_mixing_dravidian_languages/__init__.py
+++ code_mixing_dravidian_languages/__init__.py
@@ -3,7 +3,7 @@ import os
 code_mixing_dravidian_languages_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT_PATH = os.path.dirname(code_mixing_dravidian_languages_ROOT_PATH)
 
-DATA_FOLDER_PATH = os.path.join(PROJECT_ROOT_PATH, "data")
+DATA_FOLDER_PATH = os.path.join(PROJECT_ROOT_PATH, "split_data")
 
 TODO = [
     "Combine Datasets",
