56a14a4bb0688926a52f69915cb7ab43043203ba
Karthik Rangasai <karthikrangasai@gmail.com>
2021-10-25 14:31:37 +0530

    Update hparams search script.

--- code_mixing_dravidian_languages/hyperparameter_search.py
+++ code_mixing_dravidian_languages/hyperparameter_search.py
@@ -2,11 +2,12 @@ import os
 import sys
 
 print(sys.prefix)
-import wandb
 from argparse import ArgumentParser
 from dataclasses import asdict
+from functools import partial
 
 import pytorch_lightning as pl
+import wandb
 from pytorch_lightning import Trainer
 
 from code_mixing_dravidian_languages.src import (
@@ -17,7 +18,7 @@ from code_mixing_dravidian_languages.src.finetuning import FINE_TUNING_MAPPING
 from code_mixing_dravidian_languages.utils import get_model_and_datamodule, get_logger
 
 
-def sweep_iteration():
+def sweep_iteration(is_hpc: bool, num_epochs: int):
     with wandb.init() as run:
         config = run.config
 
@@ -46,13 +47,16 @@ def sweep_iteration():
 
         model, datamodule = get_model_and_datamodule(classifier_configuration)
         wandb_logger = get_logger(logger_configuration)
+
+        hardware_settings = {"gpus": 1}
+        if is_hpc:
+            hardware_settings.update({"num_nodes": 2, "accelerator": "ddp"})
+
         trainer = Trainer(
             logger=wandb_logger,
             callbacks=callbacks,
-            gpus=1,
-            num_nodes=2,
-            accelerator="ddp",
-            max_epochs=3,
+            max_epochs=num_epochs,
+            **hardware_settings,
         )
 
         wandb_logger.watch(model)
@@ -63,54 +67,71 @@ def sweep_iteration():
         wandb.finish()
 
 
-sweep_config = {
-    "method": "random",  # Random search
-    "metric": {
-        # We want to maximize val_accuracy
-        "name": "val_accuracy_epoch",
-        "goal": "maximize",
-    },
-    "parameters": {
-        "backbone": {
-            # Choose from pre-defined values
-            "values": ["ai4bharat/indic-bert", "xlm-roberta-base", "xlm-roberta-large"]
-        },
-        "batch_size": {
-            # Choose from pre-defined values
-            "values": [4, 8, 16]
-        },
-        "max_length": {
-            # Choose from pre-defined values
-            "values": [128, 258, 384, 512]
-        },
-        "lr": {
-            # log uniform distribution between exp(min) and exp(max)
-            "distribution": "log_uniform",
-            "min": -18.420680744,  # exp(-18.420680744) = 1e-8
-            "max": 0,  # exp(0) = 1
-        },
-        "finetuning_strategy": {
-            # Choose from pre-defined values
-            "values": [None, "freeze"]
-        },
-    },
-}
-
-
 def main():
     pl.seed_everything(42)
 
     parser = ArgumentParser()
-    parser.add_argument("--count", default=1, type=int)
+    parser.add_argument("--hpc", action="store_true")
+    parser.add_argument("--sweep_id", type=str, default=None, required=False)
+    parser.add_argument("-t", "--trials", type=int, default=1, required=False)
+    parser.add_argument("-e", "--epochs", type=int, default=1, required=False)
+    parser.add_argument(
+        "-s",
+        "--sampler",
+        type=str,
+        default="random",
+        required=False,
+        choices=["random", "grid", "bayes"],
+    )
     args = parser.parse_args()
 
+    sweep_config = {
+        "method": args.sampler,  # Random search
+        "metric": {
+            # We want to maximize val_accuracy
+            "name": "val_accuracy_epoch",
+            "goal": "maximize",
+        },
+        "parameters": {
+            "backbone": {
+                # Choose from pre-defined values
+                "values": [
+                    "ai4bharat/indic-bert",
+                    "xlm-roberta-base",
+                    "xlm-roberta-large",
+                ]
+            },
+            "batch_size": {
+                # Choose from pre-defined values
+                "values": [4, 8, 16]
+            },
+            "max_length": {
+                # Choose from pre-defined values
+                "values": [128, 258, 384, 512]
+            },
+            "lr": {
+                # log uniform distribution between exp(min) and exp(max)
+                "distribution": "log_uniform",
+                "min": -18.420680744,  # exp(-18.420680744) = 1e-8
+                "max": 0,  # exp(0) = 1
+            },
+            "finetuning_strategy": {
+                # Choose from pre-defined values
+                "values": [None, "freeze"]
+            },
+        },
+    }
+
     wandb.login()
-    sweep_id = wandb.sweep(sweep_config, project="Code_Mixing_Sentiment_Classifier")
+    if args.sweep_id is None:
+        sweep_id = wandb.sweep(sweep_config, project="Code_Mixing_Sentiment_Classifier")
+    else:
+        sweep_id = args.sweep_id
     wandb.agent(
         sweep_id,
-        function=sweep_iteration,
+        function=partial(sweep_iteration, is_hpc=args.hpc, num_epochs=args.epochs),
         project="Code_Mixing_Sentiment_Classifier",
-        count=args.count,
+        count=args.trials,
     )
 
 
