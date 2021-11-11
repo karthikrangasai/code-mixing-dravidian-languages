4e079865e4bee3fc5d32e0e7b3f64d7ed80adaeb
Karthik Rangasai <karthikrangasai@gmail.com>
2021-11-11 22:12:02 +0530

    Add new dataset - Codalab Codemix hate speech detection.

--- code_mixing_dravidian_languages/src/data.py
+++ code_mixing_dravidian_languages/src/data.py
@@ -1,5 +1,7 @@
 import os
+from types import LambdaType
 import pandas as pd
+from copy import deepcopy
 from pathlib import Path
 from typing import Dict
 
@@ -13,12 +15,59 @@ from code_mixing_dravidian_languages.src.preprocess import (
     _category_preprocess_fn,
 )
 
+DATA_METADATA = {
+    "fire_2020":{
+        "name": "FIRE_2020",
+        "languages": ["all", "tamil", "malayalam", "kannada"],
+        "data_folder_path": os.path.join(DATA_FOLDER_PATH, "fire_2020_sentiment"),
+        "num_classes": 5,
+        "category_mapping" : {
+            "Positive ": 0,
+            "Positive": 0,
+            "Negative": 1,
+            "not-Tamil": 2,
+            "not-Kannada": 2,
+            "not-malayalam": 2,
+            "unknown_state": 3,
+            "unknown state": 3,
+            "Mixed feelings": 4,
+            "Mixed_feelings": 4,
+        },
+        "filenames":{
+            "train": lambda language: f"{language}_sentiment_full_train.tsv",
+            "dev": lambda language: f"{language}_sentiment_full_dev.tsv",
+            "test": lambda language: f"{language}_sentiment_full_test_withlabels.tsv",
+        },
+    },
+    "codalab":{
+        "name": "CODALAB_HATE_SPEECH",
+        "languages": ["tamil"],
+        "data_folder_path": os.path.join(DATA_FOLDER_PATH, "codalab_hate_speech"),
+        "num_classes": 2,
+        "category_mapping" : {
+            "NOT": 0,
+            "OFF": 1,
+        },
+        "filenames":{
+            "train": lambda language: f"{language}_offensive_train.tsv",
+            "dev": lambda language: f"{language}_offensive_dev.tsv",
+        },
+    }
+}
 
 class CodeMixingSentimentClassifierDataset(Dataset):
-    def __init__(self, filepath: str, tokenizer: str, language: str, max_token_len: int = 128):
+    def __init__(
+        self, 
+        filepath: str, 
+        tokenizer: str, 
+        language: str, 
+        category_mapping: Dict[str, int],
+        max_token_len: int = 128
+    ):
         self.data = pd.read_csv(filepath, sep="\t")
         self.tokenizer = tokenizer
         self.language = language
+        self.category_mapping = category_mapping
         self.max_token_len = max_token_len
 
     def __len__(self):
@@ -28,7 +77,7 @@ class CodeMixingSentimentClassifierDataset(Dataset):
         data_row = self.data.iloc[index]
 
         preprocessed_text = _text_preprocess_fn(data_row["text"], self.language)
-        label = _category_preprocess_fn(data_row["category"])
+        label = self.category_mapping[data_row["category"]]
 
         sample = self.tokenizer.encode_plus(
             preprocessed_text,
@@ -59,15 +108,16 @@ class CodeMixingSentimentClassifierDataModule(LightningDataModule):
 
     def __init__(
         self,
+        dataset: str,
         backbone: str = "ai4bharat/indic-bert",
         language: str = "tamil",
         batch_size: int = 8,
         max_length: int = 128,
         padding: str = "max_length",
         num_workers: int = 0,
-        data_folder_path: str = DATA_FOLDER_PATH,
     ):
-        assert language in ["tamil", "kannada", "malayalam", "all"]
+        self.dataset_metadata = deepcopy(DATA_METADATA[dataset])
+        assert language in self.dataset_metadata["languages"]
         super().__init__()
         self.backbone = backbone
         self.tokenizer = AutoTokenizer.from_pretrained(backbone)
@@ -76,47 +126,32 @@ class CodeMixingSentimentClassifierDataModule(LightningDataModule):
         self.language = language
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.data_folder_path = data_folder_path
+        
 
     @staticmethod
-    def _check_for_files(data_folder_path: str, language: str) -> bool:
-        train_file = Path(os.path.join(data_folder_path, language, f"{language}_sentiment_full_train.tsv"))
-        val_file = Path(os.path.join(data_folder_path, language, f"{language}_sentiment_full_dev.tsv"))
-        test_file = Path(
-            os.path.join(
-                data_folder_path,
-                language,
-                f"{language}_sentiment_full_test_withlabels.tsv",
-            )
-        )
-
-        return (
-            (train_file.exists() and train_file.is_file())
-            and (val_file.exists() and val_file.is_file())
-            and (test_file.exists() and test_file.is_file())
-        )
+    def _check_for_files(data_folder_path: str, language: str, files: Dict[str, LambdaType]) -> bool:
+        train_file = Path(os.path.join(data_folder_path, language, files["train"](language)))
+        val_file = Path(os.path.join(data_folder_path, language, files["dev"](language)))
+        return (train_file.exists() and train_file.is_file()) and (val_file.exists() and val_file.is_file())
 
     def prepare_data(self):
-        assert CodeMixingSentimentClassifierDataModule._check_for_files(self.data_folder_path, self.language)
+        data_folder_path = self.dataset_metadata["data_folder_path"]
+        language = self.language
+        files = self.dataset_metadata["filenames"]
+        assert CodeMixingSentimentClassifierDataModule._check_for_files(
+            data_folder_path=data_folder_path,
+            language=language,
+            files=files,
+        )
 
     def setup(self, stage):
         # Load the data
+        data_folder_path = self.dataset_metadata["data_folder_path"]
+        language = self.language
+        files = self.dataset_metadata["filenames"]
         datafiles = {
-            "train": os.path.join(
-                self.data_folder_path,
-                self.language,
-                f"{self.language}_sentiment_full_train.tsv",
-            ),
-            "val": os.path.join(
-                self.data_folder_path,
-                self.language,
-                f"{self.language}_sentiment_full_dev.tsv",
-            ),
-            "test": os.path.join(
-                self.data_folder_path,
-                self.language,
-                f"{self.language}_sentiment_full_test_withlabels.tsv",
-            ),
+            "train": os.path.join(data_folder_path, language, files["train"](language)),
+            "val": os.path.join(data_folder_path, language, files["dev"](language)),
         }
 
         self.datasets: Dict[str, Dataset] = {
@@ -125,22 +160,16 @@ class CodeMixingSentimentClassifierDataModule(LightningDataModule):
                 tokenizer=self.tokenizer,
                 language=self.language,
                 max_token_len=self.max_length,
+                category_mapping=self.dataset_metadata["category_mapping"],
             ),
             "val": CodeMixingSentimentClassifierDataset(
                 filepath=datafiles["val"],
                 tokenizer=self.tokenizer,
                 language=self.language,
                 max_token_len=self.max_length,
-            ),
-            "test": CodeMixingSentimentClassifierDataset(
-                filepath=datafiles["test"],
-                tokenizer=self.tokenizer,
-                language=self.language,
-                max_token_len=self.max_length,
+                category_mapping=self.dataset_metadata["category_mapping"],
             ),
         }
-
-        # setattr(self, "datasets", datasets)
 
     def train_dataloader(self):
         return DataLoader(
@@ -156,16 +185,6 @@ class CodeMixingSentimentClassifierDataModule(LightningDataModule):
     def val_dataloader(self):
         return DataLoader(
             self.datasets["val"],
-            batch_size=self.batch_size,
-            collate_fn=default_data_collator,
-            drop_last=True,
-            num_workers=self.num_workers,
-            pin_memory=True,
-        )
-
-    def test_dataloader(self):
-        return DataLoader(
-            self.datasets["test"],
             batch_size=self.batch_size,
             collate_fn=default_data_collator,
             drop_last=True,
--- code_mixing_dravidian_languages/train.py
+++ code_mixing_dravidian_languages/train.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from typing import Union
+from typing import Optional, Union
 import wandb
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
@@ -15,27 +15,28 @@ from code_mixing_dravidian_languages.src import (
     CodeMixingSentimentClassifierDataModule,
     CodeMixingSentimentClassifier,
 )
+from code_mixing_dravidian_languages.src.data import DATA_METADATA
 from code_mixing_dravidian_languages.src.finetuning import FINE_TUNING_MAPPING
 
 
 def main(
+    backbone: str,
     learning_rate: float,
+    lr_scheduler: str,
+    num_warmup_steps:Union[int, float],
+    dataset: str,
+    language: str,
     batch_size: int,
-    max_epochs: int,
+    max_length: int,
+    num_workers: int,
     operation_type: str,
     finetuning_strategy: str,
-    backbone: str,
-    language: str,
-    max_length: int,
+    max_epochs: int, 
     gpus: int,
-    disable_wandb: bool,
     ckpt_path: str,
     wandb_run_id: str,
-    num_workers: int,
+    disable_wandb: bool,
     debug: bool,
-    data_folder_path: str,
-    lr_scheduler: str,
-    num_warmup_steps: Union[int, float],
 ) -> None:
 
     # Setup Data
@@ -46,14 +47,14 @@ def main(
         max_length=max_length,
         padding="max_length",
         num_workers=num_workers,
-        data_folder_path=data_folder_path,
+        dataset=dataset,
     )
 
     # Setup Model
     if ckpt_path == "":
         model = CodeMixingSentimentClassifier(
             backbone=backbone,
-            num_classes=5,
+            num_classes=DATA_METADATA[dataset]["num_classes"],
             learning_rate=learning_rate,
             batch_size=batch_size,
             lr_scheduler=lr_scheduler,
@@ -129,7 +130,6 @@ def main(
         wandb_logger.watch(model)
 
     trainer.fit(model, datamodule=datamodule)
-    trainer.test(model, datamodule=datamodule)
 
     if not disable_wandb:
         wandb.finish()
@@ -139,22 +139,22 @@ if __name__ == "__main__":
     pl.seed_everything(42)
     parser = ArgumentParser()
 
-    parser.add_argument("--learning_rate", default=1e-5, type=float)
-    parser.add_argument("--batch_size", default=8, type=int)
-    parser.add_argument("--max_epochs", default=10, type=int)
-    parser.add_argument("--operation_type", default="train", type=str)
-    parser.add_argument("--finetuning_strategy", default=None, type=str, required=False)
     parser.add_argument("--backbone", default="ai4bharat/indic-bert", type=str, required=False)
-    parser.add_argument("--language", default="all", type=str, required=False)
-    parser.add_argument("--max_length", default=256, type=int)
+    parser.add_argument("--learning_rate", default=1e-5, type=float)
     parser.add_argument("--lr_scheduler", type=str, default="linear", required=False)
     parser.add_argument("--num_warmup_steps", type=Union[int, float], default=0.1, required=False)
+    parser.add_argument("--dataset", required=True, type=str, choices=["fire_2020", "codalab"])
+    parser.add_argument("--language", required=True, type=str, choices=["all", "tamil", "malayalam", "kannada"])
+    parser.add_argument("--batch_size", default=8, type=int)
+    parser.add_argument("--max_length", default=256, type=int)
+    parser.add_argument("--num_workers", type=int, required=False, default=0)
+    parser.add_argument("--operation_type", default="train", type=str)
+    parser.add_argument("--finetuning_strategy", default=None, type=str, required=False)
+    parser.add_argument("--max_epochs", default=10, type=int)
     parser.add_argument("--gpus", choices=[0, 1, 12, 21, 22], default=1, type=int, required=False)
-    parser.add_argument("--disable_wandb", action="store_true", required=False)
-    parser.add_argument("--data_folder_path", required=False, default=DATA_FOLDER_PATH)
     parser.add_argument("--ckpt_path", type=str, required=False, default="")
     parser.add_argument("--wandb_run_id", type=str, required=False, default=None)
-    parser.add_argument("--num_workers", type=int, required=False, default=0)
+    parser.add_argument("--disable_wandb", action="store_true", required=False)
     parser.add_argument("--debug", action="store_true", required=False)
     args = parser.parse_args()
 
@@ -162,21 +162,25 @@ if __name__ == "__main__":
         print("If loading from checkpoint, provide a wandb run id as well.")
 
     main(
-        learning_rate=args.learning_rate,
-        batch_size=args.batch_size,
-        max_epochs=args.max_epochs,
-        operation_type=args.operation_type,
-        finetuning_strategy=args.finetuning_strategy,
         backbone=args.backbone,
-        language=args.language,
-        max_length=args.max_length,
-        gpus=args.gpus,
-        disable_wandb=args.disable_wandb,
-        ckpt_path=args.ckpt_path,
-        wandb_run_id=args.wandb_run_id,
-        num_workers=args.num_workers,
-        debug=args.debug,
-        data_folder_path=args.data_folder_path,
+        learning_rate=args.learning_rate,
         lr_scheduler=args.lr_scheduler,
         num_warmup_steps=args.num_warmup_steps,
+        
+        dataset=args.dataset,
+        language=args.language,
+        batch_size=args.batch_size,
+        max_length=args.max_length,
+        num_workers=args.num_workers,
+        
+        operation_type=args.operation_type,
+        finetuning_strategy=args.finetuning_strategy,
+        
+        max_epochs=args.max_epochs,
+        gpus=args.gpus,
+        ckpt_path=args.ckpt_path,
+        wandb_run_id=args.wandb_run_id,
+        
+        disable_wandb=args.disable_wandb,
+        debug=args.debug,
     )
